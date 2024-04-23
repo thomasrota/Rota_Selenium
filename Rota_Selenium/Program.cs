8a07a5d6f41b473b18772ac7e48b2dce5ab700dd
rota.20196 <rota.20196@E2-9-PC11>
2024-04-23 12:07:07 +0200

    <>

--- Rota_Selenium/Program.cs
+++ Rota_Selenium/Program.cs
@@ -11,23 +11,27 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 
-
 namespace Rota_Selenium
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string artista;
-            Console.Write("Inserisci nome dell'artista: ");
-            artista = Console.ReadLine();
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl($"https://open.spotify.com/search/{artista}/artists");
-
+            string artista; Console.Write("Inserisci nome dell'artista: "); artista = Console.ReadLine(); IWebDriver driver = new ChromeDriver(); driver.Navigate().GoToUrl($"https://open.spotify.com/search/{artista}/artists");
 
             Thread.Sleep(2000);
             IWebElement div = driver.FindElement(By.ClassName("main-view-container"));
-            Console.WriteLine(div.Text);
+            IWebElement firstArtistLink = div.FindElement(By.CssSelector("div[data-encore-id='card']"));
+
+            // Click on the first artist element link
+            firstArtistLink.Click();
+
+            string artistURL = firstArtistLink.GetAttribute("href");
+            artistURL += "/discography/album";
+
+
+            driver.Navigate().GoToUrl(artistURL);
+
             /*var uls = div.FindElements(By.TagName("ul"));
             List<string> autori = new List<string>();
             //variabile foreach
